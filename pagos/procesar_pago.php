742dfa79c4202a2bca1629088a873286346f51dc
243105092escuela-eng <243105092escuela@gmail.com>
2026-03-05 10:59:39 -0600

    Update procesar_pago.php

--- pagos/procesar_pago.php
+++ pagos/procesar_pago.php
@@ -55,12 +55,12 @@ if ($accion === 'enviar_codigo') {
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'gimori66@gmail.com';
-        $mail->Password   = 'njobvdpzfwrjtrss';
+        $mail->Username   = '@gmail.com';
+        $mail->Password   = '';
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port       = 587;
 
-        $mail->setFrom('gimori66@gmail.com', 'GameMaster México');
+        $mail->setFrom('@gmail.com', 'GameMaster México');
         $mail->addAddress($correo);
 
         $mail->isHTML(true);
